18745416f46d51c05524a28c502e4c46da368e61
DeepakGandhi2007 <95476025+DeepakGandhi2007@users.noreply.github.com>
2025-11-26 01:03:30 +0530

    Add expiresAt field to User model

--- models/User.js
+++ models/User.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
   dailyCount: Number,
   lastReset: Date,
   deviceId: { type: String, default: null },
-  token: { type: String, default: null }
+  token: { type: String, default: null },
+  expiresAt: { type: Date }
 });
 
 module.exports = mongoose.model("User", userSchema);
